a8cbc3fdaa8ce6fe4646128c0c9bea19ef69f630
soumik12345 <19soumik.rakshit96@gmail.com>
2020-11-29 04:06:28 +0530

    Added Residual Resizing Modules

---
+++ mirnet/model/residual_resizing_modules.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+
+
+def down_sampling_module(input_tensor):
+    """Downsampling Module"""
+    channels = list(input_tensor.shape)[-1]
+    main_branch = tf.keras.layers.Conv2D(
+        channels, kernel_size=(1, 1))(input_tensor)
+    main_branch = tf.keras.layers.PReLU()(main_branch)
+    main_branch = tf.keras.layers.Conv2D(
+        channels, kernel_size=(3, 3), padding='same')(main_branch)
+    main_branch = tf.keras.layers.ReLU()(main_branch)
+    main_branch = tf.keras.layers.MaxPooling2D()(main_branch)
+    main_branch = tf.keras.layers.Conv2D(
+        channels * 2, kernel_size=(1, 1))(main_branch)
+    skip_branch = tf.keras.layers.MaxPooling2D()(input_tensor)
+    skip_branch = tf.keras.layers.Conv2D(
+        channels * 2, kernel_size=(1, 1))(skip_branch)
+    return tf.keras.layers.Add()([skip_branch, main_branch])
+
+
+def up_sampling_module(input_tensor):
+    """Upsampling Module"""
+    channels = list(input_tensor.shape)[-1]
+    main_branch = tf.keras.layers.Conv2D(
+        channels, kernel_size=(1, 1))(input_tensor)
+    main_branch = tf.keras.layers.PReLU()(main_branch)
+    main_branch = tf.keras.layers.Conv2D(
+        channels, kernel_size=(3, 3), padding='same')(main_branch)
+    main_branch = tf.keras.layers.ReLU()(main_branch)
+    main_branch = tf.keras.layers.UpSampling2D()(main_branch)
+    main_branch = tf.keras.layers.Conv2D(
+        channels // 2, kernel_size=(1, 1))(main_branch)
+    skip_branch = tf.keras.layers.UpSampling2D()(input_tensor)
+    skip_branch = tf.keras.layers.Conv2D(
+        channels // 2, kernel_size=(1, 1))(skip_branch)
+    return tf.keras.layers.Add()([skip_branch, main_branch])
