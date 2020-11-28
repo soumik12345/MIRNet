b265254c4a16738d69808377ef84d720b927b14f
soumik12345 <19soumik.rakshit96@gmail.com>
2020-11-29 03:16:58 +0530

    Added selective kernel feature fusion block

---
+++ mirnet/model/skff.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+
+def selective_kernel_feature_fusion(
+        multi_scale_feature_1, multi_scale_feature_2, multi_scale_feature_3):
+    """Selective Kernel Feature Fusion Block"""
+    channels = list(multi_scale_feature_1.shape)[-1]
+    combined_feature = tf.keras.layers.Add([
+        multi_scale_feature_1, multi_scale_feature_2, multi_scale_feature_3])
+    gap = tf.keras.layers.GlobalAveragePooling2D()(combined_feature)
+    channel_wise_statistics = tf.reshape(gap, shape=(-1, 1, 1, channels))
+    compact_feature_representation = tf.keras.layers.ReLU()(
+        tf.keras.layers.Conv2D(
+            filters=channels // 8, kernel_size=(1, 1)
+        )(channel_wise_statistics)
+    )
+    feature_descriptor_1 = tf.keras.layers.Softmax()(
+        tf.keras.layers.Conv2D(channels, kernel_size=(1, 1))(compact_feature_representation)
+    )
+    feature_descriptor_2 = tf.keras.layers.Softmax()(
+        tf.keras.layers.Conv2D(channels, kernel_size=(1, 1))(compact_feature_representation)
+    )
+    feature_descriptor_3 = tf.keras.layers.Softmax()(
+        tf.keras.layers.Conv2D(channels, kernel_size=(1, 1))(compact_feature_representation)
+    )
+    feature_1 = multi_scale_feature_1 * feature_descriptor_1
+    feature_2 = multi_scale_feature_2 * feature_descriptor_2
+    feature_3 = multi_scale_feature_3 * feature_descriptor_3
+    aggregated_feature = tf.keras.layers.Add()([feature_1, feature_2, feature_3])
+    return aggregated_feature
