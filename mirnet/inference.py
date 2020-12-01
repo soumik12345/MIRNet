bd93e8c7edbe16d9a29c5077afc1b7eaa1b97aff
soumik12345 <19soumik.rakshit96@gmail.com>
2020-12-01 11:32:56 +0530

    Added inference

---
+++ mirnet/inference.py
@@ -0,0 +1,36 @@
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from .model import mirnet_model
+
+
+class Inferer:
+
+    def __init__(self):
+        self.model = None
+
+    def build_model(self, num_rrg: int, num_mrb: int, channels: int, weights_path: str):
+        self.model = mirnet_model(num_rrg, num_mrb, channels)
+        self.model.load_weights(weights_path)
+
+    def infer(self, image_path, image_resize_factor=1):
+        original_image = Image.open(image_path)
+        width, height = original_image.size
+        original_image = original_image.resize(
+            (
+                width // image_resize_factor,
+                height // image_resize_factor
+            ),
+            Image.ANTIALIAS)
+        image = tf.keras.preprocessing.image.img_to_array(original_image)
+        image = image.astype('float32') / 255.0
+        image = np.expand_dims(image, axis=0)
+        output = self.model.predict(image)
+        output_image = output[0] * 255.0
+        output_image = output_image.clip(0, 255)
+        output_image = output_image.reshape(
+            (np.shape(output_image)[0], np.shape(output_image)[1], 3)
+        )
+        output_image = Image.fromarray(np.uint8(output_image))
+        original_image = Image.fromarray(np.uint8(original_image))
+        return original_image, output_image
